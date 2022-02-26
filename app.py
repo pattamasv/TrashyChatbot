9f5453f1247e1700e9ee6d14e672ae55436f10c3
Pattamas Wiratchanee <pattamas.vrn@g.swu.ac.th>
2022-02-26 23:24:42 +0700

    update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ print('model loaded!')
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+DB_URI = app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
 #db = SQLAlchemy(app)
